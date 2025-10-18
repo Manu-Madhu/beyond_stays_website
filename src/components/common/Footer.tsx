66ef549d401907bb18b4bad78c88022c65394b24
Manu-Madhu <manum66466@gmail.com>
2025-10-19 02:45:53 +0530

    update

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
   return (
     <>
       {/* Top Marque section */}
-      <div className="top overflow-hidden border-t-2 border-t-black  md:hidden mb-2 mt-1">
+      <div className=" overflow-hidden border-t-2 border-t-black  md:hidden mb-2 mt-1">
         <Marquee gradient={false} speed={100} pauseOnHover={true}>
           <h1 className="titleHeader text-[50px] md:text-[170px] text-gray-900 text-center whitespace-nowrap ">
             # BEYOND STAYS &nbsp; # BEYOND STAYS &nbsp; # BEYOND STAYS &nbsp;
@@ -19,7 +19,7 @@ const Footer = () => {
       </div>
 
       {/* bottom Marque section */}
-      <div className="top overflow-hidden border-t-2 border-t-black hidden md:flex">
+      <div className=" overflow-hidden  border-t-black hidden md:flex">
         <Marquee gradient={false} speed={100} pauseOnHover={true}>
           <h1 className="titleHeader text-[50px] md:text-[120px] text-gray-900 text-center leading-[1] whitespace-nowrap mb-0 mt-0 overflow-hidden">
             # BEYOND STAYS &nbsp; # BEYOND STAYS &nbsp; # BEYOND STAYS &nbsp;
